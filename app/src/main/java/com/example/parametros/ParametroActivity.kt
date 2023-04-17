a0230a49df331817524c874f850c90b89777f00d
Rafagenaro <rafaelgenarovitor@gmail.com>
2023-04-17 12:56:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/parametros/ParametroActivity.kt
@@ -0,0 +1,24 @@
+package com.example.parametros
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class ParametroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parametro)
+
+        val i = Intent(this, MainActivity::class.java)
+        var res = findViewById<TextView>(R.id.txtParametro)
+        var par = intent.getStringExtra("parametro")
+        var btn = findViewById<Button>(R.id.btnBack)
+        res.setText(par)
+
+        btn.setOnClickListener {
+            startActivity(i)
+        }
+    }
+}
